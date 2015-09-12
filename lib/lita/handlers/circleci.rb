31d90f3058779e8f2f28d43c7dbdef31e7f26881
luciano <marcelinolucianom@gmail.com>
2015-09-12 13:52:26 -0300

    get branch last build status

--- lib/lita/handlers/circleci.rb
+++ lib/lita/handlers/circleci.rb
@@ -1,7 +1,24 @@
+require 'circleci'
+
 module Lita
   module Handlers
     class Circleci < Handler
       # insert handler code here
+
+      CircleCi.configure do |config|
+        config.token = ENV["CIRCLECI_TOKEN"] || ""
+      end
+      ORG = ENV['ORGANIZATION'] || 'NASA'
+      PROJECT = ENV['PROJECT'] || 'spaceship'
+
+
+
+      route(/^circle status (.+)$/, :get_status, command: true)
+
+      def get_status(message)
+        response = CircleCi::Project.build_branch(ORG, PROJECT, message.match_data[1])
+        # message.reply "Last build status for this branch: #{response.body['status']}"
+      end
 
       Lita.register_handler(self)
     end
--- spec/lita/handlers/circleci_spec.rb
+++ spec/lita/handlers/circleci_spec.rb
@@ -1,4 +1,31 @@
 require "spec_helper"
 
 describe Lita::Handlers::Circleci, lita_handler: true do
+
+  ## Routes
+  it { is_expected.to route_command('circle status some-branch').to(:get_status) }
+
+  ## Commands
+  describe '#get_status' do
+    let(:organization) { 'NASA' }
+    let(:project) { 'spaceship' }
+    let(:branch) { 'feature-branch' }
+    let(:message) { 'circle status feature-branch' }
+
+    before do
+      allow_any_instance_of(CircleCi::Project).to receive(:build_branch).and_return(false)
+    end
+
+    it 'calls circleci gem with correct parameters' do
+      expect(CircleCi::Project).to receive(:build_branch).with(organization, project, branch)
+      send_command(message)
+    end
+
+    context 'when the build is invalid' do
+    end
+
+    context 'when the build is valid' do
+    end
+  end
+
 end
