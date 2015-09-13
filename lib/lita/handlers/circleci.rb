668cdba3763cb5dcc166fa2afbdfda57ca664f12
luciano <marcelinolucianom@gmail.com>
2015-09-13 18:28:35 -0300

    handle response

--- lib/lita/handlers/circleci.rb
+++ lib/lita/handlers/circleci.rb
@@ -4,7 +4,6 @@ module Lita
   module Handlers
     class Circleci < Handler
       # config :organization_name
-      # config :project_name
       CircleCi.configure do |config|
         config.token = ENV["CIRCLECI_TOKEN"] || ""
       end
@@ -14,7 +13,19 @@ module Lita
 
       def get_status(message)
         response = CircleCi::Project.recent_builds_branch(ORG, message.match_data[1], message.match_data[2])
-        message.reply "Last build status for this branch: #{response.body.first['status']}"
+        message.reply(handle_response(response))
+      end
+
+      private
+      def handle_response(response)
+        if response.errors.present?
+          error_message = JSON.parse(response.errors[0].message)['message']
+          error_message
+        elsif response.body.empty?
+          "Branch not found"
+        else
+          "Last build status for this branch: #{response.body.first['status']}"
+        end
       end
 
       Lita.register_handler(self)
