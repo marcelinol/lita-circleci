1829f0b35e023cb0485fc8836f9159c0211882ff
luciano <marcelinolucianom@gmail.com>
2015-09-13 18:03:42 -0300

    get the build status and not build it

--- lib/lita/handlers/circleci.rb
+++ lib/lita/handlers/circleci.rb
@@ -3,20 +3,20 @@ require 'circleci'
 module Lita
   module Handlers
     class Circleci < Handler
-      #TODO: puts config on config file
+      # config :organization_name
+      # config :project_name
       CircleCi.configure do |config|
         config.token = ENV["CIRCLECI_TOKEN"] || ""
       end
+
       ORG = ENV['ORGANIZATION'] || 'NASA'
       PROJECT = ENV['PROJECT'] || 'spaceship'
-
-
 
       route(/^circle status (.+)$/, :get_status, command: true)
 
       def get_status(message)
-        response = CircleCi::Project.build_branch(ORG, PROJECT, message.match_data[1])
-        # message.reply "Last build status for this branch: #{response.body['status']}"
+        response = CircleCi::Project.recent_builds_branch(ORG, PROJECT, message.match_data[1])
+        message.reply "Last build status for this branch: #{response.body.first['status']}"
       end
 
       Lita.register_handler(self)
