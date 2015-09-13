a3e59ac2ac254d2f1821309ad9e30b7ba22ace53
luciano <marcelinolucianom@gmail.com>
2015-09-13 18:27:06 -0300

    brand new specs

--- spec/lita/handlers/circleci_spec.rb
+++ spec/lita/handlers/circleci_spec.rb
@@ -3,29 +3,46 @@ require "spec_helper"
 describe Lita::Handlers::Circleci, lita_handler: true do
 
   ## Routes
-  it { is_expected.to route_command('circle status some-branch').to(:get_status) }
+  it { is_expected.to route_command('circle status project_name/some-branch').to(:get_status) }
 
   ## Commands
   describe '#get_status' do
-    let(:organization) { 'NASA' }
-    let(:project) { 'spaceship' }
+    let(:organization_name) { 'NASA' }
+    let(:project_name) { 'spaceship' }
     let(:branch) { 'feature-branch' }
-    let(:message) { 'circle status feature-branch' }
+    let(:message) { 'circle status spaceship/feature-branch' }
 
     before do
-      allow_any_instance_of(CircleCi::Project).to receive(:build_branch).and_return(false)
+      allow_any_instance_of(CircleCi::Project).to receive(:recent_builds_branch).and_return(false)
     end
 
     it 'calls circleci gem with correct parameters' do
-      expect(CircleCi::Project).to receive(:build_branch).with(organization, project, branch)
+      expect(CircleCi::Project).to receive(:recent_builds_branch).with(organization_name, project_name, branch)
       send_command(message)
     end
 
-    context 'when the build is invalid' do
+    context 'when the build is valid' do
+      it { expect(replies.last).to include('Last build status for this branch:') }
     end
 
-    context 'when the build is valid' do
+    context 'with invalid information' do
+      context 'of project' do
+        let(:message) { 'circle status wrong_project/feature-branch' }
+
+        it 'replies with an error message' do
+          expect(replies.last).to eq('Project not found')
+        end
+      end
+
+      context 'of branch' do
+        let(:message) { 'circle status spaceship/inexistent-branch' }
+
+        it 'replies with an error message' do
+          expect(replies.last).to eq('Branch not found')
+        end
+      end
     end
+
   end
 
 end
