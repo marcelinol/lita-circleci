684cff22509a010427ae3c5aebcdda8970918539
luciano <marcelinolucianom@gmail.com>
2015-09-13 18:27:59 -0300

    receive project on message

--- lib/lita/handlers/circleci.rb
+++ lib/lita/handlers/circleci.rb
@@ -8,14 +8,12 @@ module Lita
       CircleCi.configure do |config|
         config.token = ENV["CIRCLECI_TOKEN"] || ""
       end
-
       ORG = ENV['ORGANIZATION'] || 'NASA'
-      PROJECT = ENV['PROJECT'] || 'spaceship'
 
-      route(/^circle status (.+)$/, :get_status, command: true)
+      route(/^circle status (.+)\/(\S+)$/, :get_status, command: true)
 
       def get_status(message)
-        response = CircleCi::Project.recent_builds_branch(ORG, PROJECT, message.match_data[1])
+        response = CircleCi::Project.recent_builds_branch(ORG, message.match_data[1], message.match_data[2])
         message.reply "Last build status for this branch: #{response.body.first['status']}"
       end
 
