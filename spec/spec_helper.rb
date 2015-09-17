9cada2e00823d538e034c011bbb07e3259a4c4e8
luciano <marcelinolucianom@gmail.com>
2015-09-16 22:37:32 -0300

    uses vcr and turn coveralls on again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-# require "simplecov"
-# require "coveralls"
-# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-#   SimpleCov::Formatter::HTMLFormatter,
-#   Coveralls::SimpleCov::Formatter
-# ]
-# SimpleCov.start { add_filter "/spec/" }
+require "simplecov"
+require "coveralls"
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start { add_filter "/spec/" }
 
 require "lita-circleci"
 require "lita/rspec"
@@ -16,5 +16,12 @@ Lita.version_3_compatibility_mode = false
 RSpec.configure do |config|
   # Use color in STDOUT
   config.color = true
+end
 
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
 end
